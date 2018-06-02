fbb0c7e9030fc1137c7e337954c5e68d4e8c4511
ryanzola <ryanzola@me.com>
2018-06-02 03:46:19 -0400

    wow, walls, floors, particle system

--- dist/bundle.js
+++ dist/bundle.js
@@ -90,6 +90,9 @@ var Decoration = function Decoration() {
   // Run the Group constructor with the given arguments
   THREE.Group.apply(this, arguments);
 
+  this.rotationSpeed = Math.random() * 0.02 + 0.005;
+  this.rotationPosition = Math.random();
+
   // A random color assignment
   var colors = ['#ff0051', '#f56762', '#a53c6c', '#f19fa0', '#72bdbf', '#47689b'];
 
@@ -107,7 +110,7 @@ var Decoration = function Decoration() {
   bauble.rotateY(Math.random() * Math.PI * 2);
   this.add(bauble);
 
-  // A cylinder to represent the top attachment
+  // A cylinder to represent the top attachement
   var shapeOne = new THREE.Mesh(addNoise(new THREE.CylinderGeometry(4, 6, 10, 6, 1), 0.5), new THREE.MeshStandardMaterial({
     color: 0xf8db08,
     flatShading: true,
@@ -120,14 +123,13 @@ var Decoration = function Decoration() {
   shapeOne.receiveShadow = true;
   this.add(shapeOne);
 
-  // A torus to represent the top hootks
+  // A Torus to represent the top hook
   var shapeTwo = new THREE.Mesh(addNoise(new THREE.TorusGeometry(2, 1, 6, 4, Math.PI), 0.2), new THREE.MeshStandardMaterial({
     color: 0xf8db08,
     flatShading: true,
     metalness: 0,
     roughness: 0.8,
     refractionRatio: 0.25
-
   }));
   shapeTwo.position.y += 13;
   shapeTwo.castShadow = true;
@@ -136,43 +138,54 @@ var Decoration = function Decoration() {
 };
 Decoration.prototype = Object.create(THREE.Group.prototype);
 Decoration.prototype.constructor = Decoration;
+Decoration.prototype.updatePosition = function () {
+  this.rotationPosition += this.rotationSpeed;
+  this.rotation.y = Math.sin(this.rotationPosition);
+};
 
 // Create a scene which will hold all our meshes to be rendered
 var scene = new THREE.Scene();
 
-// Create a camera
-var camera = new THREE.PerspectiveCamera(60, //field of view
-window.innerWidth / window.innerHeight, // aspect ratio
-0.1, // near clipping plane
-1000 // far clipping plane
+// Create and position a camera
+var camera = new THREE.PerspectiveCamera(60, // Field of view
+window.innerWidth / window.innerHeight, // Aspect ratio
+0.1, // Near clipping pane
+1000 // Far clipping pane
 );
 
 // Reposition the camera
-camera.position.set(0, 30, 50);
+camera.position.set(-100, 100, 50);
 
-// Point the camera
-camera.lookAt(0, 15, 0);
+// Point the camera at a given coordinate
+camera.lookAt(new THREE.Vector3(0, 15, 0));
 
-// Create a remderer
-var renderer = new THREE.WebGLRenderer({
-  antialias: true
-});
+// Create a renderer
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 
-// Renderer size should be the same as the window
+// Size should be the same as the window
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Set a near-white clear color (default is black)
+// Set a near white clear color (default is black)
 renderer.setClearColor(0xfff6e6);
 
 // Enable shadow mapping
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-// Append to the documnet
+window.addEventListener('resize', function () {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+});
+
+// Append to the document
 document.body.appendChild(renderer.domElement);
 
-// Add an ambient light
-var ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+// Add an ambient lights
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 scene.add(ambientLight);
 
 // Add a point light that will cast shadows
@@ -183,46 +196,120 @@ pointLight.shadow.mapSize.width = 1024;
 pointLight.shadow.mapSize.height = 1024;
 scene.add(pointLight);
 
-// A basic material that shows the ground
-var shadowMaterial = new THREE.ShadowMaterial({ color: 0xeeeeee });
-shadowMaterial.opacity = 0.5;
-var groundMesh = new THREE.Mesh(new THREE.BoxGeometry(100, .1, 100), shadowMaterial);
-groundMesh.receiveShadow = true;
-scene.add(groundMesh);
+var underFloor = new THREE.Mesh(new THREE.BoxGeometry(308, 2, 308), new THREE.MeshStandardMaterial({ color: 0xcccccc, roughness: 0.8 }), new THREE.ShadowMaterial({ color: 0x000000, opacity: 0.5 }));
+underFloor.position.y -= 1;
+scene.add(underFloor);
 
-// Add some new instance of our decoration
+var wall1 = new THREE.Mesh(new THREE.BoxGeometry(308, 2, 308), new THREE.MeshStandardMaterial({ color: 0xcccccc, roughness: 0.8 }), new THREE.ShadowMaterial({ color: 0x000000, opacity: 0.5 }));
+wall1.rotation.x = Math.PI / 2;
+wall1.position.y = 308 / 2;
+wall1.position.z = 308 / 2;
+scene.add(wall1);
+
+var planeGroup = new THREE.Group();
+
+for (var i = 0; i < 9; i++) {
+  planeGroup.add(new THREE.Mesh(new THREE.BoxGeometry(100, 2, 100), new THREE.MeshStandardMaterial({ color: 0x002233 }), new THREE.ShadowMaterial({ color: 0x000000, opacity: 0.5 })));
+}
+
+var coords = [-102, 0, 102];
+var xIndex = 0;
+var zIndex = 0;
+planeGroup.children.forEach(function (plane, index) {
+  plane.position.x = coords[xIndex];
+  plane.position.z = coords[zIndex];
+  zIndex++;
+  if (index % 3 === 0) {
+    xIndex++;
+  }
+
+  if (xIndex === 3) {
+    xIndex = 0;
+  }
+
+  if (zIndex === 3) {
+    zIndex = 0;
+  }
+});
+
+scene.add(planeGroup);
+
+var decorations = [];
+
+// Add some new instances of our decoration
 var decoration1 = new Decoration();
 decoration1.position.y += 10;
 scene.add(decoration1);
+decorations.push(decoration1);
 
 var decoration2 = new Decoration();
 decoration2.position.set(20, 15, -10);
-decoration2.scale.set(0.8, 0.8, 0.8);
+decoration2.scale.set(.8, .8, .8);
 scene.add(decoration2);
+decorations.push(decoration2);
 
 var decoration3 = new Decoration();
 decoration3.position.set(-20, 20, -12);
 scene.add(decoration3);
+decorations.push(decoration3);
 
+var program = function program(context) {
+  context.beginPath();
+  context.arc(0, 0, 0.5, 0, Math.PI * 2, true);
+  context.fill();
+};
+
+var group = new THREE.Group();
+scene.add(group);
+for (var i = 0; i < 1500; i++) {
+  var material = new THREE.SpriteMaterial({
+    color: Math.random() * 0x808008 + 0x808080,
+    lights: true
+
+  });
+  var particle = new THREE.Sprite(material);
+  particle.position.x = Math.random() * 2000 - 1000;
+  particle.position.y = Math.random() * 2000 - 1000;
+  particle.position.z = Math.random() * 2000 - 1000;
+  particle.scale.x = particle.scale.y = Math.random() * 10 + 5;
+  group.add(particle);
+}
+
+// Render the scene/camera combnation
 renderer.render(scene, camera);
 
-// Add an orbit control which allows us to more around the scene
+// Add an orbit control which allows us to move around the scene. See the three.js example for more details
+// https://github.com/mrdoob/three.js/blob/dev/examples/js/controls/OrbitControls.
 var controls = new OrbitControls(camera, renderer.domElement);
 controls.target = new THREE.Vector3(0, 15, 0);
 controls.maxPolarAngle = Math.PI / 2;
-controls.addEventListener('change', function () {
+
+requestAnimationFrame(render);
+
+function render() {
+  controls.update();
+
+  // Update the decoration positions
+  for (var d = 0; d < decorations.length; d++) {
+    decorations[d].updatePosition();
+  }
+
+  // Render the scene / camera combination
   renderer.render(scene, camera);
-});
+
+  // Repeat
+  requestAnimationFrame(render);
+}
 
 /**
- * Helper function to add random noise to geometry vertixes
- *
- * @param geometry The geometry to alter
- * @param noiseX Amount of noise on the X axis
- * @param noiseY Amount of noise on the Y axis
- * @param noiseZ Amount of noise on the Z axis
- * @returns the geometry object
- */
+* Helper function to add random noise to geometry vertixes
+*
+* @param geometry The geometry to alter
+* @param noiseX Amount of noise on the X axis
+* @param noiseY Amount of noise on the Y axis
+* @param noiseZ Amount of noise on the Z axis
+* @returns the geometry object
+*/
 function addNoise(geometry, noiseX, noiseY, noiseZ) {
 
   noiseX = noiseX || 2;
